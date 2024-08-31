7e129b6020b6acee37cd63b15f80962d12467c5b
JinkyXiang <109024403+JinkyXiang@users.noreply.github.com>
2024-08-31 23:09:47 +1200

    some change in ui

--- src/components/PreferenceAndWorkoutPlan.js
+++ src/components/PreferenceAndWorkoutPlan.js
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faDumbbell, faFlag, faMagicWandSparkles, faClock
+  faDumbbell,
+  faFlag,
+  faMagicWandSparkles,
+  faClock,
 } from "@fortawesome/free-solid-svg-icons";
 
 const PreferenceAndWorkoutPlan = () => {
@@ -38,7 +41,9 @@ const PreferenceAndWorkoutPlan = () => {
             <h5 className="card-title">Preferences</h5>
             <div className="d-flex justify-content-between">
               <div className="d-flex flex-column align-items-center mb-3">
-                <label><FontAwesomeIcon icon={faClock} /> Time</label>
+                <label>
+                  <FontAwesomeIcon icon={faClock} /> Time
+                </label>
                 <div>
                   <input
                     type="radio"
@@ -61,7 +66,9 @@ const PreferenceAndWorkoutPlan = () => {
               </div>
               <div className="border-start mx-3"></div> {/* Divider */}
               <div className="d-flex flex-column align-items-center mb-3">
-                <label><FontAwesomeIcon icon={faFlag} /> Type</label>
+                <label>
+                  <FontAwesomeIcon icon={faFlag} /> Type
+                </label>
                 <div>
                   <input
                     type="radio"
@@ -84,7 +91,9 @@ const PreferenceAndWorkoutPlan = () => {
               </div>
               <div className="border-start mx-3"></div> {/* Divider */}
               <div className="d-flex flex-column align-items-center mb-3">
-                <label><FontAwesomeIcon icon={faDumbbell} /> Equipment</label>
+                <label>
+                  <FontAwesomeIcon icon={faDumbbell} /> Equipment
+                </label>
                 <div>
                   <input
                     type="checkbox"
@@ -112,9 +121,22 @@ const PreferenceAndWorkoutPlan = () => {
         <div className="col-12 text-center">
           {/* Generate Button */}
           <button
-            className="btn btn-primary"
+            className="btn"
             onClick={generateWorkoutPlan}
-            style={{ padding: "10px", width: "50%" }}
+            style={{
+              backgroundColor: "#ee964b", // Button color
+              color: "#fcfce1", // Text color
+              padding: "10px",
+              width: "50%",
+              border: "none",
+              transition: "background-color 0.2s ease",
+            }}
+            onMouseDown={(e) =>
+              (e.currentTarget.style.backgroundColor = "#062f5b")
+            }
+            onMouseUp={(e) =>
+              (e.currentTarget.style.backgroundColor = "#083d77")
+            }
           >
             <FontAwesomeIcon icon={faMagicWandSparkles} /> Generate
           </button>
@@ -122,7 +144,10 @@ const PreferenceAndWorkoutPlan = () => {
       </div>
 
       <div className="row mt-4">
-        <div className="card border" style={{ height: '20vh', overflow: 'hidden' }}>
+        <div
+          className="card border"
+          style={{ height: "20vh", overflow: "hidden" }}
+        >
           <div className="card-body">
             <h5 className="card-title">My Workout Plan</h5>
             {workoutPlan && (
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,30 +11,42 @@ const Sidebar = () => {
   const [isExpanded, setIsExpanded] = useState(true); // State to control sidebar expansion
 
   const toggleSidebar = () => {
-    setIsExpanded(!isExpanded); 
+    setIsExpanded(!isExpanded);
   };
 
   return (
     <div
-      className={`d-flex flex-column flex-shrink-0 p-3 text-white bg-dark`}
+      className={`d-flex flex-column flex-shrink-0 p-3 `}
       style={{
         width: isExpanded ? "280px" : "80px",
         transition: "width 0.3s",
-        overflow: "hidden"
+        overflow: "hidden",
+        backgroundColor: "#083d77", // Sidebar background color
+        color: "#fcfce1", // Default text color
       }}
     >
       <div className="d-flex justify-content-center align-items-center">
         <a
           href="/"
           className="d-flex align-items-center text-white text-decoration-none"
+          style={{ color: "#fcfce1" }} // Link text color
         >
-          {isExpanded && <span className="fs-4">Looking Great</span>}
+          {isExpanded && (
+            <span className="fs-4" style={{ color: "#fcfce1" }}>
+              Looking Great
+            </span>
+          )}
         </a>
-        <button className="btn btn-dark" onClick={toggleSidebar}>
+        <button
+          className="btn "
+          onClick={toggleSidebar}
+          style={{ color: "#fcfce1" }} // Button icon color
+          backgroundColor="#083d77" // Button background color
+        >
           <FontAwesomeIcon icon={faBars} />
         </button>
       </div>
-      {isExpanded && <hr />}
+      {isExpanded && <hr style={{ borderColor: "#fcfce1" }} />}
       {isExpanded && (
         <div className="text-center mb-4">
           <img
@@ -43,32 +55,87 @@ const Sidebar = () => {
             className="rounded-circle mb-2"
             style={{ width: "100px", height: "100px" }}
           />
-          <h5>John</h5>
-          <p className="small">Los Angeles, CA</p>
-          <p className="small">Client est. 2/23/17</p>
-          <p className="small">Height: 5'8"</p>
+          <h5 style={{ color: "#fcfce1" }}>John</h5>
+          <p className="small" style={{ color: "#fcfce1" }}>
+            Los Angeles, CA
+          </p>
+          <p className="small" style={{ color: "#fcfce1" }}>
+            Client est. 2/23/17
+          </p>
+          <p className="small" style={{ color: "#fcfce1" }}>
+            Height: 5'8"
+          </p>
         </div>
       )}
-      <hr />
+      <hr style={{ borderColor: "#fcfce1" }} />
       <ul className="nav nav-pills flex-column mb-auto text-center">
         <li className="nav-item">
-          <a href="#" className={`nav-link ${isExpanded ? '' : 'text-center'} py-3`} aria-current="page">
+          <a
+            href="#"
+            className={`nav-link ${isExpanded ? "" : "text-center"} py-3`}
+            aria-current="page"
+            style={{
+              color: "#fcfce1", // Default link color
+            }}
+            activeStyle={{
+              backgroundColor: "#ee964b", // Active link background color
+              color: "#fcfce1",
+            }}
+          >
             <FontAwesomeIcon icon={faHome} className="fs-4" />
             {isExpanded && <span className="ms-2">Dashboard</span>}
           </a>
         </li>
         <li>
-          <a href="#" className={`nav-link text-white ${isExpanded ? '' : 'text-center'} py-3`}>
+          {/* <a
+            href="#"
+            className={`nav-link text-white ${
+              isExpanded ? "" : "text-center"
+            } py-3`}
+          >
+            <FontAwesomeIcon icon={faTachometerAlt} className="fs-4" />
+            {isExpanded && <span className="ms-2">Analytics</span>}
+          </a> */}
+          <a
+            href="#"
+            className={`nav-link ${isExpanded ? "" : "text-center"} py-3`}
+            style={{
+              color: "#fcfce1", // Default link color
+            }}
+            activeStyle={{
+              backgroundColor: "#ee964b", // Active link background color
+              color: "#fcfce1",
+            }}
+          >
             <FontAwesomeIcon icon={faTachometerAlt} className="fs-4" />
             {isExpanded && <span className="ms-2">Analytics</span>}
           </a>
         </li>
         <li>
-          <a href="#" className={`nav-link text-white ${isExpanded ? '' : 'text-center'} py-3`}>
+          {/* <a
+            href="#"
+            className={`nav-link text-white ${
+              isExpanded ? "" : "text-center"
+            } py-3`}
+          >
+            <FontAwesomeIcon icon={faSignOut} className="fs-4" />
+            {isExpanded && <span className="ms-2">Sign out</span>}
+          </a> */}
+          <a
+            href="#"
+            className={`nav-link ${isExpanded ? "" : "text-center"} py-3`}
+            style={{
+              color: "#fcfce1", // Default link color
+            }}
+            activeStyle={{
+              backgroundColor: "#ee964b", // Active link background color
+              color: "#fcfce1",
+            }}
+          >
             <FontAwesomeIcon icon={faSignOut} className="fs-4" />
             {isExpanded && <span className="ms-2">Sign out</span>}
           </a>
-        </li>        
+        </li>
       </ul>
     </div>
   );
