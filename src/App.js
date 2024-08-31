f1cc84d6f4eb2f0c7220a61ed5b769dec0840ae6
Phone Ko <phoneko@gmail.com>
2024-08-31 18:39:39 +1200

    merge

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import React from 'react';
 import 'firebase/firestore'; 
-import firebaseApp from './firebaseConfig';
 import { BrowserRouter as Router} from 'react-router-dom';
 import {Routes, Route} from 'react-router-dom';
 import Home from './page/Home'
+import ConnectStrava from './page/ConnectStrava';
+import StravaAuth from './page/StravaAuth';
 
-const db = firebaseApp; // Get a reference to the Firestore database
 
 function App() {
   return (
@@ -15,6 +15,8 @@ function App() {
       <section>                              
           <Routes>                                                       
               <Route path="/" element={<Home/>}/>
+              <Route path="/connect-strava" element={<ConnectStrava />}/>
+              <Route path="/strava-auth" element={<StravaAuth />}/>
           </Routes>                    
       </section>
     </div>
---
+++ src/page/ConnectStrava.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function redirectToStrava() {
+  const clientId = '133480'; // replace with your actual client id
+  const redirectUri =
+  process.env.NODE_ENV === 'production'
+    ? 'https://lookinggreat-a4f55.web.app/strava-auth' // your production URL
+    : 'http://localhost:3000/strava-auth'; // development URL
+  const scope = 'read,activity:read'; // adjust scopes as needed
+  
+  const authUrl = `https://www.strava.com/oauth/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scope}&approval_prompt=force`;
+
+  window.location.href = authUrl;
+}
+
+function ConnectStrava() {
+  return (
+    <div>
+      <button onClick={redirectToStrava}>Connect with Strava</button>
+    </div>
+  );
+}
+
+export default ConnectStrava;
