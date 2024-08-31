a8eb77b1905f4ef420290ccd02297933459703ad
Ziyu Xie <zxie722@aucklanduni.ac.nz>
2024-08-31 17:21:01 +1200

    Update ConnectStrava.js

--- src/page/ConnectStrava.js
+++ src/page/ConnectStrava.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function redirectToStrava() {
   const clientId = '133480'; // replace with your actual client id
-  const redirectUri = 'http://localhost:3000/strava-auth'; // or your production URL
+  const redirectUri =
+  process.env.NODE_ENV === 'production'
+    ? 'https://lookinggreat-a4f55.web.app/strava-auth' // your production URL
+    : 'http://localhost:3000/strava-auth'; // development URL
   const scope = 'read,activity:read'; // adjust scopes as needed
   
   const authUrl = `https://www.strava.com/oauth/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scope}&approval_prompt=force`;
